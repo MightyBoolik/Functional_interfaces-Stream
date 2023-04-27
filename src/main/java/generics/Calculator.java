cb1cdaa41888e93306594482c85285ed99402bc4
MightyBoolik <bkuchaev@gmail.com>
2023-04-27 16:52:48 +0500

    edit calc methods

--- src/main/java/generics/Calculator.java
+++ src/main/java/generics/Calculator.java
@@ -2,27 +2,75 @@ package generics;
 
 public class Calculator {
 
-    public static <T extends Number, V extends Number> double sum(T t, V v) {
-        double num1 = t.doubleValue();
-        double num2 = v.doubleValue();
-        return num1 + num2;
+    public static <T extends Number> T sum(T t, T v) {
+        if (t instanceof Integer) {
+            Integer result = (Integer)t + (Integer)v;
+            return (T) result;
+        }
+        if (t instanceof Long) {
+            Long result = (Long)t + (Long)v;
+            return (T) result;
+        }
+        if (t instanceof Float) {
+            Float result = (Float)t + (Float)v;
+            return (T) result;
+        } else {
+            Double result = (Double)t + (Double)v;
+            return (T) result;
+        }
     }
 
-    public static <T extends Number, V extends Number> double substraction(T t, V v) {
-        double num1 = t.doubleValue();
-        double num2 = v.doubleValue();
-        return num1 - num2;
+    public static <T extends Number> T substraction(T t, T v) {
+        if (t instanceof Integer) {
+            Integer result = (Integer)t - (Integer)v;
+            return (T) result;
+        }
+        if (t instanceof Long) {
+            Long result = (Long)t - (Long)v;
+            return (T) result;
+        }
+        if (t instanceof Float) {
+            Float result = (Float)t - (Float)v;
+            return (T) result;
+        } else {
+            Double result = (Double)t - (Double)v;
+            return (T) result;
+        }
     }
 
-    public static <T extends Number, V extends Number> double multiply(T t, V v) {
-        double num1 = t.doubleValue();
-        double num2 = v.doubleValue();
-        return num1 * num2;
+    public static <T extends Number, V extends Number> T multiply(T t, T v) {
+        if (t instanceof Integer) {
+            Integer result = (Integer)t * (Integer)v;
+            return (T) result;
+        }
+        if (t instanceof Long) {
+            Long result = (Long)t * (Long)v;
+            return (T) result;
+        }
+        if (t instanceof Float) {
+            Float result = (Float)t * (Float)v;
+            return (T) result;
+        } else {
+            Double result = (Double)t * (Double)v;
+            return (T) result;
+        }
     }
 
-    public static <T extends Number, V extends Number> double divide(T t, V v) {
-        double num1 = t.doubleValue();
-        double num2 = v.doubleValue();
-        return num1 / num2;
+    public static <T extends Number, V extends Number> T divide(T t, V v) {
+        if (t instanceof Integer) {
+            Integer result = (Integer)t / (Integer)v;
+            return (T) result;
+        }
+        if (t instanceof Long) {
+            Long result = (Long)t / (Long)v;
+            return (T) result;
+        }
+        if (t instanceof Float) {
+            Float result = (Float)t / (Float)v;
+            return (T) result;
+        } else {
+            Double result = (Double)t / (Double)v;
+            return (T) result;
+        }
     }
 }
--- src/main/java/generics/Main.java
+++ src/main/java/generics/Main.java
@@ -2,13 +2,18 @@ package generics;
 
 public class Main {
     public static void main(String[] args) {
+
         Integer [] array = {1, 2, 3, 4, 5};
+        Double [] array1 = {1.2, 2.2, 3.2, 4.2, 5.2};
         GenericForArray<Integer> generic = new GenericForArray<>(array);
+        GenericForArray<Double> generic1 = new GenericForArray<>(array1);
+        System.out.println(generic1.getMinimum());
+        System.out.println(generic1.getMaximum());
         System.out.println(generic.getMinimum());
         System.out.println(generic.getMaximum());
-        System.out.println(Calculator.sum(5,5));
-        System.out.println(Calculator.multiply(5,7.5));
-        System.out.println(Calculator.substraction(15,7.5));
+        System.out.println(Calculator.sum(9223372036854775805L,2L));
+        System.out.println(Calculator.multiply(5,7));
+        System.out.println(Calculator.substraction(15.5,7.5));
         System.out.println(Calculator.divide(15,3));
     }
 }
